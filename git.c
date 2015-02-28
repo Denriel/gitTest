28bc1b7568415f761dbf8b797e5247f7dfa9d0ea
Folio <starbi@hotmail.fr>
2015-02-28 12:49:15 +0100

    Hello

---
+++ git.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int tab[4], i = 0;
+	tab[0] = 1;
+	tab[1] = 5;
+	tab[2] = 8;
+	tab[3] = 3;
+    
+    for (i = 0 ; i <4 ; i++)
+    {
+	     printf("%d\n", tab[i]);
+    }
+	return (0);
+}
